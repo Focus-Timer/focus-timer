3fd340fbc78d011ceecb67ccbe337ffd7514bdb7
Alex Kruger <alexander.kruger@bbd.co.za>
2024-05-30 08:16:48 +0200

    Inc dec buttons

--- app/public/javascripts/tasks.js
+++ app/public/javascripts/tasks.js
@@ -7,3 +7,15 @@ addTaskCancelButton.onclick = () => {
     addTaskDialog.style.visibility = 'hidden';
     addTaskDialog.style.display = 'none';
 }
+
+increasePomodorosButton.onclick = () => {
+    if (pomodorosInput.value < 100) {
+        ++pomodorosInput.value;
+    }  
+}
+
+decreasePomodorosButton.onclick = () => {
+    if (pomodorosInput.value > 0) {
+        --pomodorosInput.value;
+    }  
+}
