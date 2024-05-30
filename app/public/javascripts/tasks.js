f9c953c6ac7609dd5458b31e6e00253ce59c91f2
Alex Kruger <alexander.kruger@bbd.co.za>
2024-05-30 13:15:36 +0200

    Add task error checking

--- app/public/javascripts/tasks.js
+++ app/public/javascripts/tasks.js
@@ -1,3 +1,16 @@
+pomodorosInput.addEventListener('input', pomodorosChangeHandler);
+addTaskSaveButton.disabled = false;
+
+function pomodorosChangeHandler() {
+    if (pomodorosInput.value > 99) {
+        pomodorosInput.value = 99;
+    } else if (pomodorosInput.value < 0) {
+        pomodorosInput.value = 0;
+    } else if (!pomodorosInput.value) {
+        pomodorosInput.value = 0;
+    }
+}
+
 addTaskButton.onclick = () => {
     addTaskDialog.style.visibility = 'visible';
     addTaskDialog.style.display = 'flex';
@@ -6,7 +19,23 @@ addTaskButton.onclick = () => {
 addTaskCancelButton.onclick = () => {
     addTaskDialog.style.visibility = 'hidden';
     addTaskDialog.style.display = 'none';
+    addTaskInput.value = '';
+    pomodorosInput.value = 0;
 }
+
+addTaskSaveButton.onclick = () => {
+    if (String(addTaskInput.value).length < 1) {
+        addTaskInputError.classList.remove('hide-component');
+    } else {
+        addTaskInputError.classList.add('hide-component');
+        //add task
+        addTaskDialog.style.visibility = 'hidden';
+        addTaskDialog.style.display = 'none';
+        addTaskInput.value = '';
+        pomodorosInput.value = 0;
+    }
+}
+
 
 increasePomodorosButton.onclick = () => {
     if (pomodorosInput.value < 100) {
