9fbadc44402c8578566e25883f7fc8a3552cc7c4
Alex Kruger <alexander.kruger@bbd.co.za>
2024-06-02 13:04:49 +0200

    Task deletion

--- app/frontend/public/scripts/tasks.js
+++ app/frontend/public/scripts/tasks.js
@@ -1,8 +1,10 @@
 pomodorosInput.addEventListener('input', pomodorosChangeHandler);
-
 addTaskSaveButton.disabled = false;
 let taskList = [];
 let currentTaskItem = 0;
+let deleteTaskVisible = false;
+let deleteTaskClicked = false;
+let taskToDelete = -1;
 
 addTaskButton.onclick = () => {
     addTaskDialog.style.visibility = 'visible';
@@ -73,10 +75,6 @@ function pomodorosChangeHandler() {
 }
 
 function updateTaskList() {
-    let checkboxes = taskListComponent.querySelectorAll('.checkbox-round');
-    checkboxes.forEach(checkbox => {
-      checkbox.removeEventListener('change', handleCheckboxChange);
-    });
 
     taskListComponent.innerHTML = '';
 
@@ -93,17 +91,17 @@ function updateTaskList() {
         }
 
         listItem.innerHTML += `<section class="task-left">
-        <input type="checkbox" class="checkbox-round" />
-        <h4>${task.taskName}</h4>    
-      </section>
-      <section class="task-right">
-        <p>${task.currentPomodoro}/${task.taskPomodoros}</p>
-        <ul class="ellipse">
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </section>`
+                                    <input type="checkbox" class="checkbox-round" />
+                                    <h4>${task.taskName}</h4>    
+                                </section>
+                                <section class="task-right">
+                                    <p>${task.currentPomodoro}/${task.taskPomodoros}</p>
+                                    <ul class="ellipse">
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    </ul>
+                                </section>`
 
         taskListComponent.appendChild(listItem);
     }
@@ -112,7 +110,59 @@ function updateTaskList() {
     checkboxes.forEach(checkbox => {
       checkbox.addEventListener('change', handleCheckboxChange);
     });
+
+    ellipses = document.querySelectorAll(".ellipse");
+    ellipses.forEach(ellipse => {
+        ellipse.addEventListener('click', function() {
+            const buttonRect = ellipse.getBoundingClientRect();
+            deleteTaskDialog.style.top = buttonRect.top + 'px';
+            deleteTaskDialog.style.left = (buttonRect.right + 8) + 'px';
+            deleteTaskDialog.style.visibility = 'visible';
+            deleteTaskDialog.style.display = 'flex';
+            deleteTaskClicked = true;
+            deleteTaskVisible = true;
+            taskToDelete = Array.from(taskListComponent.children).indexOf(ellipse.closest('li'));
+      });
+    });
 }
+
+document.addEventListener('click', function(event) {
+    if (deleteTaskVisible) {
+        if (!event.target.matches('.delete-task-btn')) {
+            if (!deleteTaskClicked) {
+                this.deleteTaskVisible = false;
+                deleteTaskDialog.style.visibility = 'hidden';
+                deleteTaskDialog.style.display = 'none';
+                taskToDelete = -1;
+            } else {
+                deleteTaskClicked = false;
+            }
+        } else if (taskToDelete !== -1) {
+            // Update current task
+            if (currentTaskItem === taskToDelete) {
+                if (taskList.length === 1) {
+                    currentTaskItem = 0;
+                } else {
+                    (currentTaskItem + 1) < taskList.length ? currentTaskItem++ : currentTaskItem--;
+                }
+            }
+
+            // Delete task
+            taskListComponent.children[taskToDelete].remove();
+            taskList.splice(taskToDelete, 1);
+            taskToDelete = -1;
+
+            // Hide popup
+            deleteTaskDialog.style.visibility = 'hidden';
+            deleteTaskDialog.style.display = 'none';
+            deleteTaskVisible = false;
+            deleteTaskClicked = false;
+                
+            updateTaskList();
+        }
+    }
+  });
+
 
 function handleCheckboxChange(event) {
     const checkbox = event.target;
@@ -123,4 +173,4 @@ function handleCheckboxChange(event) {
     } else {
         listItem.classList.remove('strike-through');
     }
-  }
+}
