9408d3b6c4fd347398de1f6169e52041e8f8aac1
yashilBBD <yashil.luckan@bbd.co.za>
2024-06-04 16:25:11 +0200

    dynamically change between prod and non prod redirects

--- app/frontend/routes/view.js
+++ app/frontend/routes/view.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var router = express.Router();
 require('dotenv').config({ override: true });
+if(process.env.MODE === 'debug') {
 
+}
 router.get('/', function (req, res, next) {
   const clientId = process.env.COGNITO_CLIENT_ID;
-  res.render('login', { clientId });
+  if(process.env.MODE !== 'production') {
+    var url = process.env.LOCAL_URL
+  } else {
+    url = process.env.HOSTED_URL;
+  }
+  res.render('login', { clientId, url });
 });
 
 // Render login page
 router.get('/login', (req, res) => {
   const clientId = process.env.COGNITO_CLIENT_ID;
-  res.render('login', { clientId });
+  if(process.env.MODE !== 'production') {
+    var url = process.env.LOCAL_URL
+  } else {
+    url = process.env.HOSTED_URL;
+  }
+  res.render('login', { clientId, url });
 });
 
 // Render signup page
@@ -21,7 +33,12 @@ router.get('/signup', (req, res) => {
 /* GET home page. */
 router.get('/home', (req, res) => {
   const clientId = process.env.COGNITO_CLIENT_ID;
-  res.render('home', { clientId });
+  if(process.env.MODE !== 'production') {
+    var url = process.env.LOCAL_URL
+  } else {
+    url = process.env.HOSTED_URL;
+  }
+  res.render('home', { clientId, url });
 });
 
 module.exports = router;
