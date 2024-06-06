5d27578cd0ec7baec92e142d62f9e93fe43612c5
Bryce <bryce.grahn@bbd.co.za>
2024-06-06 14:20:01 +0200

    filter cors

--- app/backend/config/cors-middleware.js
+++ app/backend/config/cors-middleware.js
@@ -1,19 +1,20 @@
+require("dotenv").config();
 
-let allowedOrigins = ["https://rudolph-sucks.projects.bbdgrad.com", "https://localhost"]
+let allowedOrigins = [process.env.HOSTED_URL]
 
 const corsOptions = {
   origin: function (origin, callback) {
     console.log(origin)
-      if (!origin) {
-          // Allow requests with no origin (like from Postman or curl)
-          callback(null, true);
-      } else if (allowedOrigins.origin.includes(origin)) {
-          // Allow if the origin is in the allowedOrigins list
-          callback(null, true);
-      } else {
-          // Reject if the origin is not in the allowedOrigins list
-          callback(new Error('Not allowed by CORS'));
-      }
+    if (!origin) {
+      // Allow requests with no origin (like from Postman or curl)
+      callback(null, true);
+    } else if (allowedOrigins.origin.includes(origin)) {
+      // Allow if the origin is in the allowedOrigins list
+      callback(null, true);
+    } else {
+      // Reject if the origin is not in the allowedOrigins list
+      callback(new Error('Not allowed by CORS'));
+    }
   }
 };
 
